ce9668dcb5a5ba013c78b84a8cc4e73a5bf118a2
Eliezer Ayertey <eliezer.ayertey@gmail.com>
2026-01-19 05:37:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9418482505-9519f",
+  "appId": "1:584922704773:web:e7dd6647b74dcdb7cbb2ec",
+  "apiKey": "AIzaSyDn0Wtfl1uWXDozguTkKUvpTw3C7ifBNzI",
+  "authDomain": "studio-9418482505-9519f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "584922704773"
+};
